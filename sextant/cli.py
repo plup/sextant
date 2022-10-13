5971c1260d56e821dbe12013fa0cd546cc9695c3
Plup <plup@plup.io>
2022-10-13 18:15:19 +0200

    feat: added ioc search among observables

--- sextant/cli.py
+++ sextant/cli.py
@@ -9,6 +9,26 @@ from rich.console import Console
 from .auth.okta import get_credentials
 from .thehive import TheHiveClient
 
+
+def format(results, fields, title):
+    """Turns list of objects in a table."""
+    # filter output
+    rows = []
+    for result in results:
+        # force returning name for files
+        if result['dataType'] == 'file':
+            result['data'] = result['attachment']['name']
+        rows.append([str(result[k]) for k in fields])
+
+    # build the table
+    table = Table(title=title)
+    for field in fields:
+        table.add_column(field)
+    for row in rows:
+        table.add_row(*row)
+
+    return table
+
 # click entrypoint
 @click.group()
 @click.option('-c', '--config', envvar='SEXTANT_CONFIG', type=click.Path(exists=True),
@@ -17,31 +37,40 @@ from .thehive import TheHiveClient
 @click.pass_context
 def main(ctx, config):
     """Navigate through cosmic events."""
-    # store the config as context
-    ctx.ensure_object(dict)
     with open(config) as f:
-       ctx.obj = yaml.safe_load(f)
+       conf = yaml.safe_load(f)
+       ctx.obj = TheHiveClient(conf['thehive'])
 
+# okta commands
 @main.command()
 def okta():
     """Test Okta authentication with Yubikey."""
     get_credentials()
     click.echo('ok')
 
-@main.command()
-@click.option('-t', '--type', is_flag=True)
+# thehive commands
+@main.group()
 @click.pass_obj
-def obs(config, type):
+def obs(thehive):
     """Manage observables."""
-    thehive = TheHiveClient(config['thehive'])
-    if type:
-        table = Table(title='Observable types')
-        rows, fields = thehive.types(table=True)
-        for field in fields:
-            table.add_column(field)
-        for row in rows:
-            table.add_row(*row)
 
-        console = Console()
-        console.print(table)
+@obs.command()
+@click.pass_obj
+def types(thehive):
+    """Display all observable types."""
+    fields = ['name', 'isAttachment', 'createdBy']
+    results = thehive.types()
+    table = format(results, fields, 'Observable types')
+    Console().print(table)
 
+@obs.command()
+@click.option('--ioc', is_flag=True)
+@click.pass_obj
+def search(thehive, ioc):
+    """Search across observables."""
+    fields = ['id', 'dataType', 'ioc', 'sighted', 'tlp', 'data']
+
+    if ioc:
+        results = thehive.iocs()
+        table = format(results, fields, 'IOCs')
+        Console().print(table)
--- sextant/thehive.py
+++ sextant/thehive.py
@@ -28,20 +28,27 @@ class TheHiveClient(object):
     def __init__(self, config):
         self.client = TheHiveApiExt(config['endpoint'], config['apikey'], cert=False)
 
-    def types(self, table=False):
-        """Get all observables grouped by type."""
+    def format(self, results, filter_fields):
+        """Returns a filtered list of objects ready for displaying."""
+        # filter fields
+        fields = ['id', 'dataType', 'sighted', 'tlp', 'data']
+        rows = []
+        for result in results:
+            rows.append([str(result[k]) for k in fields])
+        return (rows, fields)
+
+    def types(self):
+        """Get all observables types available."""
         resp = self.client.get_observable_types()
         if resp.status_code != 200:
             raise Exception('error')
+        return resp.json()
 
-        results = resp.json()
-
-        if table:
-            # filter fields
-            fields = ['name', 'isAttachment', 'createdBy']
-            rows = []
-            for result in results:
-                rows.append([str(result[k]) for k in fields])
-            return (rows, fields)
-
-        return results
+    def iocs(self):
+        """Get all IOCs."""
+        query = Eq('ioc', True)
+        resp = self.client.find_observables(query=query, sort=[], range='all')
+        if resp.status_code != 200:
+            # manage bad request
+            raise Exception('error')
+        return resp.json()
