962a9643b934efd2c7fb89943cdaceb01b10ea19
Plup <plup@plup.io>
2023-05-17 14:53:07 +0200

    feat: implemented search and savedsearch methods for splunk

--- sextant/splunk.py
+++ sextant/splunk.py
@@ -12,7 +12,8 @@ class SplunkPlugin(Plugin):
         # register commands
         parser = parsers.add_parser('splunk', help='Splunk')
         parser.set_defaults(func=self.routes)
-        parser.add_argument('--search', nargs=1, help='Run a search')
+        parser.add_argument('--search', nargs='?', help='Run a search')
+        parser.add_argument('--savedsearch', nargs='?', help='Find saved searches')
 
         # get splunk params
         self.endpoint = kwargs.get('endpoint')
@@ -35,18 +36,41 @@ class SplunkPlugin(Plugin):
     def routes(self, ns):
         if ns.search:
             return self.search(ns.search)
+        if ns.savedsearch:
+            return self.savedsearch(ns.savedsearch)
 
     def check(self):
         r = self.session.get(f'{self.endpoint}:8089/services/apps/local')
         r.raise_for_status()
         return True
 
-    def search(self, query):
-        """Search."""
-        payload = {'search': query, 'output_mode': 'json'}
-        r = self.session.get(f'{self.endpoint}:8089/services/search/jobs/export', data=payload)
-        r.raise_for_status()
-        print(r.json())
+    def search(self, query, max_count=100):
+        """Run search queries."""
+        try:
+            payload = {'search': query, 'output_mode': 'json', 'max_count': max_count}
+            r = self.session.post(f'{self.endpoint}:8089/services/search/jobs/export', data=payload)
+            r.raise_for_status()
+            print(r.text)
 
-    def indexes(self):
-        return self.service.indexes.get_default()
+        except requests.exceptions.HTTPError as e:
+            print(f"Error: {r.json()['messages'][0]['text']}")
+
+    def savedsearch(self, username=None):
+        """
+        Find saved searches.
+        Support filtering on username.
+        """
+        try:
+            payload = {'output_mode': 'json'}
+            # build filters
+            if username:
+                payload['search'] = f'eai:acl.owner={username}'
+            r = self.session.get(f'{self.endpoint}:8089/services/saved/searches', params=payload)
+            r.raise_for_status()
+            print(r.text)
+
+        except requests.exceptions.HTTPError as e:
+            if e.response.status_code == 400:
+                print(f"Error: {r.json()['messages'][0]['text']}")
+            else:
+                print(e)
